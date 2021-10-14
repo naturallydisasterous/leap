ac0140dfd314a42247727363ab4a5a4d23fcc699
Aaron-Mann <aaron.mann@outlook.com>
2021-10-13 19:38:24 -0700

    build and libs

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <ola/DmxBuffer.h>
+#include <ola/Logging.h>
+#include <ola/client/StreamingClient.h>
+#include <ola/thread/Thread.h>
+#include <ola/Callback.h>
+
+using namespace std;
+
+int main() {
+
+  return 0;
+}
